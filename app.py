151e5b24273d5eaf03f50a4dff1af39532084089
scott <scottluu95@gmail.com>
2023-06-29 01:05:27 -0400

    change to litestar from fastapi

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-from pathlib import Path
-
-import aiofiles
-from fastapi import FastAPI
-from starlette.middleware.gzip import GZipMiddleware
-from starlette.responses import HTMLResponse
-from starlette.staticfiles import StaticFiles
-
-_BUILD_DIR = Path(__file__).parent / "frontend" / "build"
-
-app = FastAPI()
-app.add_middleware(GZipMiddleware, minimum_size=1000)
-app.mount("/static", StaticFiles(directory=str(_BUILD_DIR / "static")), name="static")
+from litestar import Litestar, get
 
 
-@app.get("/", response_class=HTMLResponse)
-async def get_root():
-    index = _BUILD_DIR / "index.html"
-    async with aiofiles.open(str(index), "r") as f:
-        return await f.read()
+@get("/")
+def hello_world() -> dict[str, str]:
+    """Handler function that returns a greeting dictionary."""
+    return {"hello": "world"}
 
 
-@app.get("/api")
-async def read_root():
-    return {"Hello": "World"}
+app = Litestar(route_handlers=[hello_world])
