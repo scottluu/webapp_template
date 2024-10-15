a0bff452f356959072577adebfbc91c50c9b1e74
Scott <scottluu@users.noreply.github.com>
2024-10-14 23:45:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from litestar import Litestar
+from litestar.static_files import create_static_files_router
 
-from file_router import FILE_ROUTER
+from api_router import API_ROUTER
 
-app = Litestar(route_handlers=[FILE_ROUTER])
+app = Litestar(
+    route_handlers=[
+        API_ROUTER,
+        create_static_files_router(
+            path="/", directories=["frontend/dist"], html_mode=True
+        ),
+    ]
+)
